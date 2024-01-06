5b71e80f3a8407e27a87172c80ce3808d3c7213c
gaston <gastoncastillo1920@gmail.com>
2024-01-06 17:39:35 -0300

    movi unas carpetas

---
+++ app.js
@@ -0,0 +1,19 @@
+
+const express= require("express");
+const app= express()
+const path= require("path");
+const port = 3040;
+app.use(express.static("public"))("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./views/home.html"))
+})
+app.get("/header",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./views/header.html"))
+})
+app.get("/login",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./views/login.html"))
+})
+app.get("/registro",(req,res)=>{
+    res.sendFile(path.join(__dirname,"./views/registro.html"))
+})
+
+app.listen(port,()=>(console.log(`corriendo puerto http://localhost:${port}`)))
