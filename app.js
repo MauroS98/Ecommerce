05cd5d6184a5d6bb143107ee30fb8ef26c895669
Lukemciel <lucasmaciel7ynine@gmail.com>
2024-01-25 23:18:12 -0300

    agregue contenido a el main de index.html y agregue estilos

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = 3030;
 app.use(express.static('public'));
 
 app.get("/" , (req , res) => {
-    res.sendFile(path.join(__dirname, "./views/home.html"));
+    res.sendFile(path.join(__dirname, "./views/index.html"));
 })
 
 app.listen(port,()=> console.log(`http://localhost:${port}`));
