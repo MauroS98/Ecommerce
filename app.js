b761d5b5d46acb856ea14ff5ece12dc3669a0bce
gaston <gastoncastillo1920@gmail.com>
2024-01-28 00:42:22 -0300

    agregue imagenes para probar el fondo mo difique login registro y home html y css

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express= require("express");
 const app= express();
 const path= require("path");
 const port = 3040;
-app.use(express.static("public"))("/",(req,res)=>{
+app.use(express.static("public"))
+
+app.get ("/",(req,res)=>{
     res.sendFile(path.join(__dirname,"./views/home.html"))
 })
-app.get("/header",(req,res)=>{
-    res.sendFile(path.join(__dirname,"./views/header.html"))
-})
+
 app.get("/login",(req,res)=>{
     res.sendFile(path.join(__dirname,"./views/login.html"))
 })
@@ -16,18 +16,3 @@ app.get("/registro",(req,res)=>{
 })
 
 app.listen(port,()=>(console.log(`corriendo puerto http://localhost:${port}`)))
-=======
-const express = require('express');
-const { prototype } = require('module');
-const app = express();
-const path = require('path');
-const port = 3030;
-
-app.use(express.static('public'));
-
-app.get("/" , (req , res) => {
-    res.sendFile(path.join(__dirname, "./views/home.html"));
-})
-
-app.listen(port,()=> console.log(`http://localhost:${port}`));
->>>>>>> develop
