664309634e33b8bd0b7ec8a004667136b02fdfe7
SSBC185 <sam.barrioscabrera@gmail.com>
2024-02-04 11:56:33 -0300

    resolviendo merge

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-const port = 3030;
+const express= require("express");
+const app= express();
+const path= require("path");
+const port = 3040;
+app.use(express.static("public"))
 
 app.use(express.static('public'));
 
-app.get("/" , (req , res) => {
+app.get("/header" , (req , res) => {
     res.sendFile(path.join(__dirname, "./views/header.html"));
 })
 
