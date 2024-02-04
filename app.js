2829b6825261333adf449870e43dae829b26e09a
SSBC185 <sam.barrioscabrera@gmail.com>
2024-02-03 22:26:30 -0300

    header

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const port = 3030;
 
 app.use(express.static('public'));
 
-app.get("/" , (req , res) => {
-    res.sendFile(path.join(__dirname, "./views/home.html"));
+app.get("/header" , (req , res) => {
+    res.sendFile(path.join(__dirname, "./views/header.html"));
 })
 
 app.listen(port,()=> console.log(`http://localhost:${port}`));
