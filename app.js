a54345b5bbc7134532e7c0000376de6dec351c4c
SSBC185 <sam.barrioscabrera@gmail.com>
2024-02-03 22:44:36 -0300

    header

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const { prototype } = require('module');
 const app = express();
 const path = require('path');
 const port = 3030;
 
 app.use(express.static('public'));
 
-app.get("/header" , (req , res) => {
+app.get("/" , (req , res) => {
     res.sendFile(path.join(__dirname, "./views/header.html"));
 })
 
