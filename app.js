86cf0aa5d7707fa127f1b1e564f481781dad7e6f
Mauro Suarez <Mauro-suarez@hotmail.com>
2024-02-04 23:05:31 -0300

    hice desktop y tablet de footer y detalles de producto

--- app.js
+++ app.js
@@ -9,5 +9,11 @@ app.use(express.static('public'));
 app.get("/" , (req , res) => {
     res.sendFile(path.join(__dirname, "./views/home.html"));
 })
+app.get("/footer" , (req , res) => {
+    res.sendFile(path.join(__dirname, "./views/footer.html"));
+})
+app.get("/detalleProducto" , (req , res) => {
+    res.sendFile(path.join(__dirname, "./views/productDetail.html"));
+})
 
 app.listen(port,()=> console.log(`http://localhost:${port}`));
