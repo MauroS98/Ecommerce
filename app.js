8fe370021f0936ba9ddbd90838af0f09b8978a90
gaston <gastoncastillo1920@gmail.com>
2024-02-13 19:51:53 -0300

    modificando login en html y css y cambie nombre de vinculo en app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.get("/header" , (req , res) => {
     res.sendFile(path.join(__dirname, "./views/header.html"));
 })
 
-app.get("/icicio",(req,res)=>{
+app.get("/inicio",(req,res)=>{
     res.sendFile(path.join(__dirname,"./views/login.html"))
 })
 app.get("/registro",(req,res)=>{
