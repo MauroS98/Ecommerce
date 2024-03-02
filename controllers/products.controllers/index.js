034d30073ef3578a0a9a71e11168336037b14524
gaston <gastoncastillo1920@gmail.com>
2024-03-02 14:44:54 -0300

    tratandode levantar puerto

--- controllers/products.controllers/index.js
+++ controllers/products.controllers/index.js
@@ -8,5 +8,6 @@ module.exports = {
     updateProduct: require('../admin.controllers/updateProduct.controller'),
     listProducts: require('../admin.controllers/listProducts.controller'),
     header: require('../header.controllers/header.controller'),
-    head: require('../header.controllers/head')
+    head: require('../header.controllers/head'),
+    create: require("../admin.controllers/createProduct.controller"),
 }
--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -2,12 +2,14 @@ const express = require('express');
 const router = express.Router()
 
 const mainControllers = require('../controllers/products.controllers')
+// const {create, store, edit, update,  } = require('../controllers/admin');
 
 router.get("/lista-producto", mainControllers.listProducts); 
 router.get("/editar-producto/:id", mainControllers.updateProduct); 
 router.get('/dashboard', mainControllers.dashboard);
-router.get("/crearProducto", mainControllers.productCreate);
-router.post("/crearProducto")
+router.get("/crearProducto",mainControllers.createProduct);
+router.post('/crear-producto', store); 
+
 
 
 module.exports = router
