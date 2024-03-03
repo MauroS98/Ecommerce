62475f1650d9b86f89c909230658ff3637118ed4
gaston <gastoncastillo1920@gmail.com>
2024-03-03 20:54:44 -0300

    arreglando la direccion de las imagenes de todas las vistas

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const router = express.Router()
-
+const multer = require("multer")
 const mainControllers = require('../controllers/products.controllers')
 const {create,store,}= require("../controllers/admin.controllers")            
+
+const storage =multer.diskStorage({
+    destination: function (req,file,cb){
+cb(null, '../../public/images/productos')
+    },
+    filename: function(req,file,cb){
+        const formatFilename =file.fieldname +'-'+ Date.now() + path.extname(file.originalname)
+        cb(null,formatFilename)
+    }
+});
+
+const upload = multer({storage})
 
 router.get("/lista-producto", mainControllers.listProducts); 
 router.get("/editar-producto/:id", mainControllers.updateProduct); 
 router.get('/dashboard', mainControllers.dashboard);
 router.get("/crear-producto",mainControllers.create);
-router.post('/crear-producto', mainControllers.store); 
+router.post('/crear-producto', upload.single("images"), mainControllers.store); 
 
 
 
