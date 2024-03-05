c4977ca0ccfa10b8c6f839d6a2ac28e3e313ed09
Lukemciel <lucasmaciel7ynine@gmail.com>
2024-03-04 21:21:03 -0300

    arregle error de controlador y detalles

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -3,6 +3,8 @@ const router = express.Router()
 
 const mainControllers = require('../controllers/products.controllers')
 
+// "/products"
+
 router.get('/cart', mainControllers.cart)
 
 router.get('/productDetail', mainControllers.productDetail)
