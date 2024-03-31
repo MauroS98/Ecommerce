cc8fc1ba70d40344923af9915571cb538a258c3b
Lukemciel <lucasmaciel7ynine@gmail.com>
2024-03-31 20:34:57 -0300

    arregle el responsive del carrito y detalle del navbar

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -2,10 +2,13 @@ const express = require('express');
 const router = express.Router()
 
 const mainControllers = require('../controllers/products.controllers')
+const cartControllers = require('../controllers/cart.controllers')
 
 // "/products"
 
-router.get('/cart', mainControllers.cart)
+router.get('/cart', cartControllers.cart)
+router.post('addToCart')
+
 
 router.get('/productDetail', mainControllers.productDetail)
 
