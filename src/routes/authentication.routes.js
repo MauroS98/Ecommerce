996c2fe185f826ff5182afd414ad7ff45699bde5
Mauro Suarez <Mauro-suarez@hotmail.com>
2024-04-02 22:14:21 -0300

    Cree carpeta de Middleware y modifique Auth controllers

--- src/routes/authentication.routes.js
+++ src/routes/authentication.routes.js
@@ -4,7 +4,9 @@ const router = express.Router()
 const mainControllers = require('../controllers/products.controllers')
 
 router.get('/login', mainControllers.login)
+router.post('/login', mainControllers.loginProcess)
 
 router.get('/register', mainControllers.register)
+router.post('/register', mainControllers.registerProcess)
 
 module.exports = router
